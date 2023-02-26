a63afa9abd4beff1b6db45e87173d48834fde7f4
Thays Ferreira <thaysferreiraua@gmail.com>
2023-02-26 20:57:34 -0300

    Create Reserva.py

---
+++ Classes/Reserva.py
@@ -0,0 +1,13 @@
+from SisLoc.classes.operacao import Operacao
+
+
+class Reserva(Operacao):
+    def __init__(self, cpf: str, codigo: int):
+        Operacao.__init__(self, cpf, codigo)
+        self._prioridade = int()
+
+    def set_prioridade(self, prioridade: int):
+        self._prioridade = prioridade
+
+    def get_prioridade(self):
+        return self._prioridade
