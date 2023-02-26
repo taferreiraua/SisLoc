6fc9ccc5abf81a834ac86c907919684c0882d1a3
Thays Ferreira <thaysferreiraua@gmail.com>
2023-02-26 20:57:02 -0300

    Create Locacao.py

---
+++ Classes/Locacao.py
@@ -0,0 +1,13 @@
+from SisLoc.classes.operacao import Operacao
+
+
+class Locacao(Operacao):
+    def __init__(self, cpf: str, codigo: int):
+        Operacao.__init__(self, cpf, codigo)
+        self._periodo = int()
+
+    def set_periodo(self, periodo):
+        self._periodo = periodo
+
+    def get_periodo(self):
+        return self._periodo
